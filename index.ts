825f927b5129c5959c26714c1b329b4043da9a0b
frankgit7 <muhammadfarrukh55@gmail.com>
2024-06-11 10:51:16 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Please guess a number between 1-10",
+    },
+]);
+console.log(answer);
+
+if(answer.userGuessNumber === randomNumber){
+    console.log("Congratulations! you guess a right number.")
+}else{
+    console.log("You guess a wrong number.");
+}
+
+
